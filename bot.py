75045f045f1fc524b6a0f5e7b40a87dffc377cdf
redcreeper14385 <mounderfod@gmail.com>
2021-08-31 17:42:48 +0100

    Fix some things after renaming

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
-import utils
+import bot_utils
 
 load_dotenv()
 bot = commands.Bot(command_prefix="t!", activity=discord.Activity(name="for commands", type=discord.ActivityType.watching))
-bot.help_command = utils.MyHelpCommand(command_attrs={'hidden':True})
+bot.help_command = bot_utils.MyHelpCommand(command_attrs={'hidden':True})
 
 @bot.event
 async def on_ready():
@@ -14,7 +14,7 @@ async def on_ready():
 
 @bot.event
 async def on_command_error(ctx, error):
-    await utils.parse_error(ctx, error)
+    await bot_utils.parse_error(ctx, error)
     
 @bot.event
 async def on_message(message):
@@ -23,11 +23,11 @@ async def on_message(message):
         ctx.reply("<irritatered:766468833564688395>")
     await bot.process_commands(message)
 
-def load_extensions(bot):
-    extensions = ['cogs.fun', 'cogs.about', 'cogs.modrinth']
+def load_extensions(client):
+    extensions = ['cogs.fun', 'cogs.utils', 'cogs.modrinth']
     if __name__ == '__main__':
         for i in extensions:
-            bot.load_extension(i)
+            client.load_extension(i)
 
 bot.load_extension('jishaku')
 load_extensions(bot)
